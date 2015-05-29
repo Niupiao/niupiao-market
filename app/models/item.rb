22be8d5b590bc2de78b202e0236ef905e527bea2
bendrews <benjamindrews@gmail.com>
2015-05-29 16:43:40 +0000

    Updated item validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,7 @@ class Item < ActiveRecord::Base
     belongs_to :user
     validates_presence_of :name
     validates_presence_of :price
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
     validates_presence_of :quantity
+    validates :quantity, numericality: { greater_than: 0 }
 end
--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -12,7 +12,13 @@ class ItemTest < ActiveSupport::TestCase
     assert_not Item.create(name: @name).valid?
     assert_not Item.create(price: @price).valid?
     assert_not Item.create(desc: @desc).valid?
-    assert Item.create( name: @name, price: @price, quantity: @quantity, desc: @desc).valid?
+    assert Item.create(name: @name, price: @price, quantity: @quantity, desc: @desc).valid?
+  end
+  
+  test "product has nonnegative price, quantity is greater than 0" do
+    assert_not Item.create(name: @name, price: -1, quantity: @quantity, desc: @desc).valid?
+    assert_not Item.create(name: @name, price: @price, quantity: 0, desc: @desc).valid?
+    assert_not Item.create(name: @name, price: @price, quantity: -1, desc: @desc).valid?
   end
 
 end
