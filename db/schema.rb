4255ca920c5fa26cf87e9420f13777e1e8fb80f6
bendrews <benjamindrews@gmail.com>
2015-05-29 19:32:24 +0000

    edits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529155528) do
+ActiveRecord::Schema.define(version: 20150529161857) do
 
   create_table "items", force: true do |t|
     t.string  "name"
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(version: 20150529155528) do
     t.string   "password_digest"
     t.string   "address"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
