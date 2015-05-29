438a7eb195dfff82a83b548084087127ad6c8383
Mohamed Soussi <dhia.soussi@gmail.com>
2015-05-29 17:14:20 +0000

    more user tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
     
     before_save{self.email = email.downcase}
-    validates :first_name, presence: true, length: {maximum:10}
-    validates :last_name, presence: true, length: {maximum: 10}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: {maximum: 32},
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+\.[a-z]+\z/i
+    validates :email, presence: true,
                 format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
                 
     validates :password, length: {minimum: 6}, presence: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,4 +28,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not dup_user.valid?
   end
   
+  test "email should have the right form" do
+    invalid_addresses = %w[modsoussi modsou.@asdh modsoussi@bar..com haha@haha.]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid "
+    end
+  end
+  
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = " "
+    assert_not @user.valid?
+  end
+  
+  test "password should be at least 6 chars long" do
+    @user.password = @password_confirmation = "a"*5
+    assert_not @user.valid?
+  end
+  
 end
