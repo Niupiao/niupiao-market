6c8eb6a8f13730fa013e4dcf78884ffccb3fd961
Mohamed Soussi <dhia.soussi@gmail.com>
2015-05-29 14:58:37 +0000

    one last change to layout tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do 
+  test "confirming home links" do 
     get root_path
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", cart_path
