7458d2d5d919a10947b61c69b92aedd42c67aa25
bendrews <benjamindrews@gmail.com>
2015-05-29 22:30:46 +0000

    Error setting users for items

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,18 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
-            redirect_to items_path
+            flash[:notice] = session[:user_id]
+            redirect_to '/items/new'
         else
-            flash[:danger] = 'Invalid email/password combination'
+            flash.now[:danger] = 'Invalid email/password combination'
             render 'new'
         end
     end
     
     def destroy
+    end
+    
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
     end
 end
