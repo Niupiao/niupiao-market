5bebe52bf2aada4b3ce419d0568bca34385684ae
Mohamed Soussi <dhia.soussi@gmail.com>
2015-06-01 17:52:37 +0000

    user cart saved, site kinda slow :s

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,7 @@ module SessionsHelper
     # Logs in the given user.
     def log_in(user)
         session[:user_id] = user.id
+        session[:cart] = sto_h(user.cart) if user.cart
     end
     
     def current_user
@@ -38,7 +39,28 @@ module SessionsHelper
     end
     
     def log_out
+        user = User.find_by(id: session[:user_id])
+        user.update_attribute(:cart, hto_s(session[:cart]))
+        clear_cart
         session.delete(:user_id)
         @current_user = nil
     end
+    
+    private
+        
+    #converts hashs to string    
+    def hto_s(hash)
+        shash = ""
+        hash.each do |id, quantity|
+            shash += id + " " + quantity.to_s + ","
+        end
+        return shash
+    end
+    
+    #reverse hto_s
+    def sto_h(shash)
+        hash = Hash[shash.split(",").map { |str| str.split}]
+        return hash
+    end
+        
 end
