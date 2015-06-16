b4f7fae383911c0288513783678818acc1014372
Ryan Kwon <rynkwn@gmail.com>
2015-06-16 14:50:45 +0000

    Improved search algorithm. Now counts score better. Orders results. Zero length search term returns all items.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,6 +30,7 @@ class ItemsController < ApplicationController
     
     def search
         @search = []
+        scores = []
        search_term = params[:search_term]
        if search_term
            search_term = search_term.downcase.gsub(/\s+/m, ' ').strip.split(" ")
@@ -38,16 +39,19 @@ class ItemsController < ApplicationController
                               item.user.first_name.downcase + " " + 
                               item.user.last_name.downcase
              score = 0
-             target_score = (search_term.size / 2) + 1 # Score must be at least 1
+             target_score = (search_term.size / 2.0).ceil # Score must be at least 1
              search_term.each do |word|
                  if(search_space.include? word)
-                     score += 1
+                     score += (search_space.scan(/#{word}/).length)
                  end
              end
              if(score >= target_score)
                @search += [item]
+               scores += [score]
              end
            end
+           p @search.sort_by.with_index{|_,i| scores[i]}
+           @search.reverse!
            return @search
        end
     end
