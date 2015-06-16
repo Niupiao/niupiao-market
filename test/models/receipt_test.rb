9a5ca9d353faa9b42cc0371b87af596d93ba44ac
Ryan Kwon <rynkwn@gmail.com>
2015-06-16 22:11:41 +0000

    Created basic Receipt model test.

--- test/models/receipt_test.rb
+++ test/models/receipt_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ReceiptTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @item_name = "test"
+    @item_quantity = 1
+    @item_type = VALID_ITEM_TYPES.first
+    @seller_name = "foo"
+    @seller_id = 1
+    @charge = 2.0
+  end
+  
+  test "valid receipt is valid" do
+    receipt = Receipt.new(item_name: @item_name, item_quantity: @item_quantity,
+                          item_type: @item_type, seller_name: @seller_name,
+                          seller_id: @seller_id, charge: @charge)
+                          
+    assert receipt.valid?, "Valid receipt is not valid."
+  end
 end
