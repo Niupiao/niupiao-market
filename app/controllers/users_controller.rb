3d08cf9430a91062b7550193c8f899425217d7a9
Ryan Kwon <rynkwn@gmail.com>
2015-06-17 16:18:29 +0000

    Set up really not-very-useful Edit screen for user information.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,19 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash.now[:success] = "Information updated"
+    else
+    end
+    render 'edit'
+  end
+  
   def create
    @user = User.new(user_params)
    @user.cart = {}  # Sets user's cart to be non-nil.
