f07e54db4f6b753cccf3c09baa4947a4b0017555
bendrews <benjamindrews@gmail.com>
2015-06-17 18:26:04 +0000

    Removed unless: provider from User password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true,
                 format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
                 
-    validates :password, length: {minimum: 6}, presence: true, unless: :provider
+    validates :password, length: {minimum: 6}, presence: true
     validates :uid, presence: true, if: :provider
     
     has_secure_password
