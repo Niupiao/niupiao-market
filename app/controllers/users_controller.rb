5c5cb5821a66614191e3d400d236fb02f5e7d194
Ryan Kwon <rynkwn@gmail.com>
2015-06-17 16:05:13 +0000

    Set up some basic authorization for Users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :destroy]
+  before_action :correct_user,   only: [:edit, :destroy]
   
   def new
     @user = User.new
@@ -32,6 +34,20 @@ class UsersController < ApplicationController
   end
 
   def index
+  end
+  
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_path
+    end
+  end
+  
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
   
   private
