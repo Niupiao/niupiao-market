6584a6428fbb34c15c07a2106fbf73f51a935575
Ryan Kwon <rynkwn@gmail.com>
2015-06-18 15:12:53 +0000

    Shortened code

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,28 +3,17 @@ module UsersHelper
   
   #  Creates a receipt for the User for the given item.
   def get_receipt(buyer, seller, item, quantity_purchased, total_charge)
-      if buyer
-        buyer.receipts_buy.create(
-                             item_name: item.name, 
-                             item_quantity: quantity_purchased,
-                             item_tags: item.tags,
-                             item_type: item.item_type,
-                             charge: total_charge,
-                             status: "Order Sent",
-                             seller: seller
-                             )
-      else
-        checkin_code = SecureRandom.urlsafe_base64 10
-        seller.receipts_sell.create(
-                              item_name: item.name,
-                              item_quantity: quantity_purchased,
-                              item_tags: item.tags,
-                              item_type: item.item_type,
-                              charge: total_charge,
-                              status: "Order Sent",
-                              checkin_code: checkin_code
-                              )
-        flash.now[:success] = "In order to keep track of your purchase, input this into the Tracker " + checkin_code
-      end
+    checkin_code = SecureRandom.urlsafe_base64 11
+    seller.receipts_sell.create(
+                          item_name: item.name,
+                          item_quantity: quantity_purchased,
+                          item_tags: item.tags,
+                          item_type: item.item_type,
+                          charge: total_charge,
+                          status: "Order Sent",
+                          checkin_code: checkin_code,
+                          buyer: buyer
+                          )
+    flash.now[:success] = "In order to keep track of your purchase, input this into the Tracker " + checkin_code
   end
 end
