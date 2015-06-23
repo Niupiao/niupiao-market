15feabf05712357345a800a0fbbf66efe6b20d13
bendrews <benjamindrews@gmail.com>
2015-06-23 23:34:42 +0000

    cleaned code

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -5,7 +5,7 @@ class DeliveriesController < ApplicationController
         if @driver = Driver.find_by(key: params[:key])
           render :json => @driver
         else
-          render :json => @driver = {message: "Invalid key"}
+          render :json => {error: "Invalid key"}
         end
       end
     end
@@ -15,9 +15,9 @@ class DeliveriesController < ApplicationController
     respond_to do |format|
       format.json do
         if Driver.find_by(key: params[:key])
-          render :json => @delivery = {deliveries: Receipt.where(claimed: 0).each}
+          render :json => @delivery = Receipt.where(claimed: 0).each
         else
-          render :json => @delivery = {message: "Invalid key"}
+          render :json => {error: "Invalid key"}
         end
       end
     end
@@ -30,12 +30,12 @@ class DeliveriesController < ApplicationController
           if @delivery = Receipt.find_by(id: params[:delivery_id])
             @delivery.update_attributes(claimed: 1)
             @delivery.save
-            render :json => @delivery = {receipt: @delivery}
+            render :json => @delivery
           else
-            render :json => @delivery = {message: "Receipt does not exist"}
+            render :json => {error: "Receipt does not exist"}
           end
         else
-          render :json => @delivery = {message: "Invalid key"}
+          render :json => {error: "Invalid key"}
         end
       end
     end
