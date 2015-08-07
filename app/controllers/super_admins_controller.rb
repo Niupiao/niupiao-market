79ebb74929de60f5d028c147eec7539346e393b4
Ryan Kwon <rynkwn@gmail.com>
2015-08-07 07:34:35 +0000

    More fleshing out of SuperAdmin.

--- app/controllers/super_admins_controller.rb
+++ app/controllers/super_admins_controller.rb
@@ -6,7 +6,16 @@ class SuperAdminsController < ApplicationController
   def crunch
     # If superadmin is logged in. Then define items. Need to also pass in the kind
     # of query that's passed in. 
-    @results = Receipt.all
+    if params[:type] == "not picked up"
+      @results = Receipt.where(status: "Order Sent")
+    elsif params[:type] == "transaction"
+      if params[:filter]
+        h = params[:filter].to_i
+        @results = Receipt.where(["status != '%s' and updated_at <= '%s'", "Complete", Time.now - h.hours])
+      else
+        @results = Receipt.all
+      end
+    end
     
     # Issue. need to structure the results table based on query type. Lots of partials.
     # Pass in the type of query. 
